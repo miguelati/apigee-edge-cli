352c8960fd20becc33dcd182a8a47b122f3581f8
Miguel Atilio Godoy <miguelgodoyg@gmail.com>
2017-12-08 13:12:42 -0300

    Add init, rewrite & apiproxy command.

--- index.js
+++ index.js
@@ -4,73 +4,24 @@ const CLI = require("clui");
 const figlet = require("figlet");
 const Preferences = require("preferences");
 const Spinner = CLI.Spinner;
-const _ = require("lodash");
 const touch = require("touch");
-const files = require("./lib/files");
 const vorpal = require('vorpal')();
 const chalk = vorpal.chalk;
-const SwaggerParser = require('swagger-parser');
-const fs = require("fs-plus");
-// Xml Classes
-const ApiProxy = require('./lib/classes/generates/ApiProxy');
+// Command Classes
+const InitCommand = require('./lib/commands/init');
+const RewirteCommand = require('./lib/commands/rewrite');
+const ApiproxyCommand = require('./lib/commands/apiproxy');
 
 clear();
 console.log(
 	chalk.yellow(
-		figlet.textSync("EDGE", { horizontaleLayout: 'full'})
+		figlet.textSync("EDGE CLI", { horizontaleLayout: 'full'})
 	)
 );
 
-vorpal
-  .command('init', 'Load swagger and create proxy with specs')
-  .action(function(args, callback) {
-
-    var auxVorpal = this;
-    var swaggers = fs.listSync("./", ["yaml", "json"]);
-
-    var flowsProcess = function(path) {
-      var flowTemplateRaw = fs.readFileSync(__dirname + "/templates/proxy_endpoint/FlowTemplate.xml");
-
-      console.log(flowTemplateRaw.toString());
-    };
-    
-    SwaggerParser.validate(swaggers[0])
-      .then(function(api) {
-        if(!fs.existsSync("./apiproxy")) {
-          var dirs = ["./apiproxy/policies/", "./apiproxy/proxies/default/flows", "./apiproxy/resoruces/jsc", "./apiproxy/targets/default/flows"];
-          var files = ["./apiproxy/" + api.info.title + ".xml", "./apiproxy/proxies/default/main.xml", "./apiproxy/targets/default/main.xml"];
-
-          for (var i = 0; i < dirs.length; i++) {
-            console.log(chalk.yellow("Create " + dirs[i]));
-            fs.makeTreeSync(dirs[i]);
-          }
-          for (var i = 0; i < files.length; i++) {
-            console.log(chalk.yellow("Create " + files[i]));
-            touch.sync(files[i]);
-          }
-        } 
-        ApiProxy.create(api);
-        auxVorpal.delimiter(chalk.green(api.info.title + "$"));
-        //console.log(JSON.stringify(api, null, 4));
-        
-        //console.log(api.paths);
-        callback();
-      })
-      .catch(function(err) {
-        console.error('Onoes! The API is invalid. ' + err.message);
-        callback();
-      });
-      
-    /*console.log(args);
-    this.log('bar');
-    this.log(files.getCurrentDirectoryBase());
-    this.log(files.getCurrentPath());
-    console.log("####");
-    console.log(__dirname);
-    */
-    
-  });
-
+InitCommand.injectCommand(vorpal);
+RewirteCommand.injectCommand(vorpal);
+ApiproxyCommand.injectCommand(vorpal);
 
 vorpal
   .delimiter('edge$')
