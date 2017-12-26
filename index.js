19f33ca95e676a499d05348b3091d751dc580b1b
Miguel Atilio Godoy <miguelgodoyg@gmail.com>
2017-12-26 00:19:44 -0300

    Refactor some classes & Begin 'policy create' command

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ const Spinner = CLI.Spinner;
 const touch = require("touch");
 const Preferences = require("preferences");
 const Watcher = require("./lib/classes/core/Watcher");
-
+const Output = require("./lib/classes/core/helpers/Output");
 
 // Command Classes
 let commands = []
 require("fs-plus").readdirSync(__dirname + '/lib/commands/').forEach(function(file) {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    //var name = file.replace('.js', '');
     commands.push(require(__dirname + '/lib/commands/' + file));
   }
 });
@@ -22,17 +21,14 @@ global.prefs = new Preferences('edge-client',{});
 global.localStorage = vorpal.localStorage;
 global.chalk = vorpal.chalk;
 global.prompt = vorpal.prompt;
+global.output = new Output();
 
 // To edit
 let watcher = new Watcher("./.tmpWatcher/", vorpal);
 watcher.start();
 
 clear();
-console.log(
-	global.chalk.yellow(
-		figlet.textSync("EDGE CLI", { horizontaleLayout: 'full'})
-	)
-);
+global.output.titleRandom(figlet.textSync("EDGE CLI", { horizontaleLayout: 'full'}));
 
 for (var i = 0; i < commands.length; i++) {
 	commands[i].injectCommand(vorpal);
