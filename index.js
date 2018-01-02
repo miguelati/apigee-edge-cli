3a2fc84a3af880f3fe1cb946c851f81fad5d945f
Miguel Atilio Godoy <miguelgodoyg@gmail.com>
2018-01-02 16:33:21 -0300

    Some fixes, Add Live Validation,

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 #! /usr/bin/env node
 const vorpal = require('vorpal')();
 const clear = require("clear");
-const CLI = require("clui");
 const figlet = require("figlet");
-const Spinner = CLI.Spinner;
-const touch = require("touch");
 const Preferences = require("preferences");
 const Watcher = require("./lib/classes/core/Watcher");
 const Output = require("./lib/classes/core/helpers/Output");
 const fs = require("fs-plus");
 const APIProxyHelper = require('./lib/classes/core/helpers/APIProxy');
+const less = require('vorpal-less');
 
 // Command Classes
 let commands = []
@@ -22,10 +20,9 @@ fs.readdirSync(__dirname + '/lib/commands/').forEach(function(file) {
 global.prefs = new Preferences('edge-client',{live: false});
 global.chalk = vorpal.chalk;
 global.prompt = vorpal.prompt;
-global.output = new Output();
-
-// To edit
-let watcher = new Watcher("./.tmpWatcher/", vorpal);
+global.output = new Output(vorpal);
+// Watcher
+global.watcher = new Watcher(["./.tmpWatcher/", "./apiproxy/"], vorpal);
 watcher.start();
 
 clear();
@@ -35,25 +32,8 @@ for (var i = 0; i < commands.length; i++) {
 	commands[i].injectCommand(vorpal);
 }
 
-const exit = vorpal.find('exit')
-if (exit) {
-  exit.remove()
-}
-
-vorpal
-  .command('exit')
-  .alias('quit')
-  .option('-f, --force', 'Forces process kill without confirmation.')
-  .description('Exits this instance of cbcluster')
-  .action(function (args) {
-    watcher.stop();
-    args.options = args.options || {}
-    args.options.sessionId = this.session.id
-    this.parent.exit(args.options)
-  })
-
 vorpal.history('edge-client');
 vorpal
   .delimiter((fs.existsSync("./apiproxy")) ? global.chalk.green(APIProxyHelper.getInfo().name + "$") : 'edge$')
+  .use(less)
   .show();
-
